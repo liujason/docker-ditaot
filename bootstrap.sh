4e35ea2491d9bb332d8e9d06f48a82a685981836
Jason Liu <jaliu@blackberry.com>
2015-09-18 15:59:57 -0400

    update vagrantfile to auto download ditaot 2.1.1 to user folder

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,3 +7,7 @@ apt-get install -y default-jdk
 curl -sSL https://get.docker.com/ | sh
 # add vagrant user to docker group to run docker
 usermod -aG docker vagrant
+
+# install dita ot 2.1.1
+curl -SL https://github.com/dita-ot/dita-ot/releases/download/2.1.1/dita-ot-2.1.1.tar.gz \
+  | tar -xzC /home/vagrant/
