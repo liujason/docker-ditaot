bae003d20b3e19181a2740ec35300058527a0c39
Jason Liu <jaliu@blackberry.com>
2015-09-22 18:12:48 -0400

    adding mongodb, install nodejs on vagrant

--- Vagrantfile
+++ Vagrantfile
@@ -1,4 +1,8 @@
 Vagrant.configure("2") do |config|
   config.vm.box="ubuntu/trusty64"
+  config.vm.network "forwarded_port", guest: 9000, host: 9000
+  config.vm.synced_folder "app/nodeapp/", "/home/vagrant/nodeapp"
+  config.vm.synced_folder "temp/mongo_data/", "/mnt/data", create: true
+  config.vm.synced_folder "temp/mongo_logs/", "/mnt/logs", create: true
   config.vm.provision :shell, path:"bootstrap.sh"
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -2,25 +2,44 @@
 
 apt-get update
 apt-get install -y default-jre
-
+# ==============================================================================
+# install nodejs
+curl -O https://raw.githubusercontent.com/nodesource/distributions/master/deb/setup_4.x
+chmod +x setup_4.x
+./setup_4.x
+apt-get install -y nodejs
+npm install -g yo grunt-cli bower
+# ==============================================================================
 # install docker (only when not installed)
 if ! hash docker 2>/dev/null; then
   curl -sSL https://get.docker.com/ | sh
   # add vagrant user to docker group to run docker
   usermod -aG docker vagrant
 fi
-# install dita ot 2.1.1
-curl -sSL https://github.com/dita-ot/dita-ot/releases/download/2.1.1/dita-ot-2.1.1.tar.gz \
-  | tar -xzC /home/vagrant/
+# ==============================================================================
+# install dita ot 2.1.1 on vagrant [optional]
+# curl -sSL https://github.com/dita-ot/dita-ot/releases/download/2.1.1/dita-ot-2.1.1.tar.gz | tar -xzC /home/vagrant/
 
+# ==============================================================================
 # build docker using tag "ot"
-cd /vagrant/dita-ot
+# Windows doesn't support symlink, so the following dita-ot is just plain text
+# cd /vagrant/dita-ot
+cd /vagrant/dita-ot-2.1.1
 docker build -t ot .
-docker run ot #this should display help
+docker run ot #this should display OT help
 
-# create ditahs temp
-mkdir /tmp/ditahs
+# Sample ot command
+# docker run \
+# -v /vagrant/tests/input/DITA1.2-spec:/tmp/ot/input:ro \
+# -v /vagrant/tests/output:/tmp/ot/output \
+# ot \
+# -i /tmp/ot/input/dita-1.2-specification.ditamap \
+# -o /tmp/ot/output \
+# -f html5
 
-# THE FOLLOWING ISN'T WORKING
-# vagrant@vagrant-ubuntu-trusty-64:/vagrant/temp/authoring$ docker run -v /vagrant/temp/authoring:/tmp/ot/input:ro -v /vagrant/temp/output:/tmp/ot/output ot -i /tmp/ot/input -o /tmp/ot/output -f html5
-# Error: [DOTA069F][FATAL] Input file '/tmp/ot/input' cannot be located or read. Ensure that file was specified properly and that you have permission to access it.
+# ==============================================================================
+# build docker-mongo
+cd /vagrant/mongodb
+docker build -t mongo .
+mkdir -p /mnt/data/mongo /mnt/logs
+docker run -d -v /mnt/data:/data -v /mnt/logs:/logs -p 27017:27017 mongo
